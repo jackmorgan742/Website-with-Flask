44f52dfeb001fe80e3bf3a74a861386c750884e2
Jack Morgan <121833759+jackmorgan742@users.noreply.github.com>
2023-01-15 17:37:24 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,5 +10,17 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    with app.app_context():
+        db.create_all()
     return app
     
